44816efe66a96285f74f661829dc7d4e1ca94c58
Sokoshy <92679596+Sokoshy@users.noreply.github.com>
2023-11-01 14:52:03 +0100

    adding postlist component

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./pages/Home";
 import Prestation from "./pages/Prestation";
 import Agence from "./pages/Agence";
+import Blog from "./pages/Blog";
 
 const router = createBrowserRouter([
   {
@@ -17,6 +18,10 @@ const router = createBrowserRouter([
   {
     path: "/agence",
     element: <Agence />,
+  },
+  {
+    path: "/blog",
+    element: <Blog />,
   },
 ]);
 
