d2a33452547f499e1a90717e8013994d5c182946
Sokoshy <92679596+Sokoshy@users.noreply.github.com>
2023-11-04 13:07:05 +0100

    Change content in toggle switch and style

--- src/components/realisation/RealisationList.jsx
+++ src/components/realisation/RealisationList.jsx
@@ -1,9 +1,41 @@
+import { useState } from "react";
+import "./style.css";
 import { RealisationParticulier } from "./RealisationParticulier";
+import { RealisationProfessionnel } from "./RealisationProfessionnel";
 
 export function RealisationList() {
+  const [checked, setChecked] = useState(false);
   return (
     <>
-      <RealisationParticulier />
+      <section className="w-[280px] mt-8 mx-auto text-[#f8f8f8] 2xl:max-w-screen-2xl xl:max-w-screen-lg md:max-w-[700px]  sm:container">
+        <div className="flex flex-col items-center xl:flex-row xl:inline-flex xl:items-end">
+          <h1 className=" text-5xl text-center font-bold 2xl:text-[5.625rem] 2xl:mt-20 xl:mt-14 xl:text-start  md:text-7xl ">
+            Réalisation
+          </h1>
+
+          <div className="xl:ml-[70px] xl:mb-6">
+            <input
+              type="checkbox"
+              checked={checked}
+              onChange={() => setChecked((prev) => !prev)}
+              id="toggle"
+              //id={`toggle ${checked ? "checked" : ""}`}
+            />
+            <label for="toggle" className="LabelBtn">
+              <p
+                className="text-lg xl:text-xl font-medium"
+                id={checked ? "toglle" : ""}
+                // className={`text-lg xl:text-xl font-medium ${
+                //   checked ? "toglle" : ""
+                // }`}
+              >
+                Particuliers<span className="ml-5">Professionels</span>
+              </p>
+            </label>
+          </div>
+        </div>
+        {checked ? <RealisationProfessionnel /> : <RealisationParticulier />}
+      </section>
     </>
   );
 }
