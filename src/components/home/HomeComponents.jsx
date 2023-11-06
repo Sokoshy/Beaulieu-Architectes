97567283c75ee37236a2e2169e26733c58f68b87
Sokoshy <92679596+Sokoshy@users.noreply.github.com>
2023-11-06 16:05:58 +0100

    add realisation secttion

--- src/components/home/HomeComponents.jsx
+++ src/components/home/HomeComponents.jsx
@@ -3,12 +3,14 @@ import { Hero } from "./components/Hero";
 import { ParleDeNous } from "./components/ParleDeNous";
 import { Presentation } from "./components/Presentation";
 import { Prestation } from "./components/Prestation";
+import { Realisation } from "./components/realisation/Realisation";
 
 export function HomeComponents() {
   return (
     <div>
       <Hero />
       <Presentation />
+      <Realisation />
       <Prestation />
       <Equipe />
       <ParleDeNous />
