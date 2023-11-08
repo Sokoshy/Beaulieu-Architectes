7b776e7857db4e637eab3c21b7ea8cc19180da78
Sokoshy <92679596+Sokoshy@users.noreply.github.com>
2023-11-08 16:27:56 +0100

    add RealisationPage and add slider for RelisationPage

---
+++ src/components/realisation/RealisationSliderPage.jsx
@@ -0,0 +1,175 @@
+import { gql, useQuery } from "@apollo/client";
+import { useParams } from "react-router-dom";
+import React from "react";
+import { useState } from "react";
+
+const GET_REALISATION = gql`
+  query RealisationContent($slug: ID!) {
+    realisation(id: $slug, idType: SLUG) {
+      slug
+      realisation {
+        photoDeLendroitAVotreArrive {
+          altText
+          sourceUrl
+        }
+        photoDeLendroitAApresEtOuPendantLaRealisation {
+          altText
+          sourceUrl
+        }
+      }
+    }
+  }
+`;
+export function RealisationSliderAvant() {
+  const [imgArriveCourante, setImgArriveCourante] = useState(0);
+
+  const handleNextArrive = () => {
+    setImgArriveCourante((prev) => (prev + 1) % imagesArrive.length);
+  };
+
+  const handlePrevArrive = () => {
+    setImgArriveCourante(
+      (prev) => (prev - 1 + imagesArrive.length) % imagesArrive.length
+    );
+  };
+  const { slug } = useParams();
+  const { loading, error, data } = useQuery(GET_REALISATION, {
+    variables: { slug },
+  });
+
+  if (loading) return;
+
+  if (error) {
+    return (
+      <p className="text-3xl mt-3 text-[#f8f8f8] text-center">
+        Erreur: {error.message}
+      </p>
+    );
+  }
+  const realisation = data.realisation;
+  const imagesArrive = realisation.realisation.photoDeLendroitAVotreArrive;
+
+  return (
+    <div className="flex items-center justify-center overflow-hidden mt-8 xl:mt-16">
+      {/* Slide précédente */}
+      <img
+        className="rounded-lg w-[112px] h-[151px] 2xl:w-[413px] 2xl:h-[518px] xl:rounded-xl xl:w-[242px] xl:h-[310px] md:w-[165px] md:h-[214px]"
+        src={
+          imagesArrive[
+            (imgArriveCourante - 1 + imagesArrive.length) % imagesArrive.length
+          ].sourceUrl
+        }
+        alt={
+          imagesArrive[
+            (imgArriveCourante - 1 + imagesArrive.length) % imagesArrive.length
+          ].altText
+        }
+      />
+      {/* Slide actuelle */}
+
+      <button
+        className="text-[#dcb854] mx-2 font-bold text-3xl lg:mt-0 xl:mx-6 md:mx-4"
+        onClick={handlePrevArrive}
+      >
+        &#10094;
+      </button>
+      <img
+        className="rounded-lg w-[160px] h-[215px] object-cover 2xl:w-[582px] 2xl:h-[712px] xl:rounded-xl xl:w-[410px] xl:h-[510px] md:w-[276px] md:h-[344px]"
+        src={imagesArrive[imgArriveCourante].sourceUrl}
+        alt={imagesArrive[imgArriveCourante].altText}
+      />
+      <button
+        className="text-[#dcb854] mx-2 font-bold text-3xl xl:mx-6 md:mx-4"
+        onClick={handleNextArrive}
+      >
+        &#10095;
+      </button>
+      {/* Slide suivante */}
+      <img
+        className="rounded-lg w-[112px] h-[151px] 2xl:w-[413px] 2xl:h-[518px] xl:rounded-xl xl:w-[242px] xl:h-[310px] md:w-[165px] md:h-[214px]"
+        src={
+          imagesArrive[(imgArriveCourante + 1) % imagesArrive.length].sourceUrl
+        }
+        alt={
+          imagesArrive[(imgArriveCourante + 1) % imagesArrive.length].altText
+        }
+      />
+    </div>
+  );
+}
+export function RealisationSliderApres() {
+  const [imgApresCourante, setImgApresCourante] = useState(0);
+
+  const handleNextApres = () => {
+    setImgApresCourante((prev) => (prev + 1) % imagesApres.length);
+  };
+
+  const handlePrevApres = () => {
+    setImgApresCourante(
+      (prev) => (prev - 1 + imagesApres.length) % imagesApres.length
+    );
+  };
+
+  const { slug } = useParams();
+
+  const { loading, error, data } = useQuery(GET_REALISATION, {
+    variables: { slug },
+  });
+
+  if (loading) return;
+
+  if (error) {
+    return (
+      <p className="text-3xl mt-3 text-[#f8f8f8] text-center">
+        Erreur: {error.message}
+      </p>
+    );
+  }
+  const realisation = data.realisation;
+  const imagesApres =
+    realisation.realisation.photoDeLendroitAApresEtOuPendantLaRealisation;
+
+  return (
+    <div className=" flex items-center justify-center overflow-hidden mt-8 xl:mt-16">
+      {/* Slide précédente */}
+
+      <img
+        className="rounded-lg w-[112px] h-[151px] 2xl:w-[413px] 2xl:h-[518px] xl:rounded-xl xl:w-[242px] xl:h-[310px] md:w-[165px] md:h-[214px]"
+        src={
+          imagesApres[
+            (imgApresCourante - 1 + imagesApres.length) % imagesApres.length
+          ].sourceUrl
+        }
+        alt={
+          imagesApres[
+            (imgApresCourante - 1 + imagesApres.length) % imagesApres.length
+          ].altText
+        }
+      />
+      {/* Slide actuelle */}
+      <button
+        className="text-[#dcb854] mx-2 font-bold text-3xl lg:mt-0 xl:mx-6 md:mx-4"
+        onClick={handlePrevApres}
+      >
+        &#10094;
+      </button>
+      <img
+        className="rounded-lg w-[160px] h-[215px] object-cover 2xl:w-[582px] 2xl:h-[712px] xl:rounded-xl xl:w-[410px] xl:h-[510px] md:w-[276px] md:h-[344px]"
+        src={imagesApres[imgApresCourante].sourceUrl}
+        alt={imagesApres[imgApresCourante].altText}
+      />
+      <button
+        className="text-[#dcb854] mx-2 font-bold text-3xl xl:mx-6 md:mx-4"
+        onClick={handleNextApres}
+      >
+        &#10095;
+      </button>
+      {/* Slide suivante */}
+      <img
+        className="rounded-lg w-[112px] h-[151px] 2xl:w-[413px] 2xl:h-[518px] xl:rounded-xl xl:w-[242px] xl:h-[310px] md:w-[165px] md:h-[214px]"
+        src={imagesApres[(imgApresCourante + 1) % imagesApres.length].sourceUrl}
+        alt={imagesApres[(imgApresCourante + 1) % imagesApres.length].altText}
+      />
+    </div>
+  );
+}
---
+++ src/pages/RealistionPage.jsx
@@ -0,0 +1,91 @@
+import { gql, useQuery } from "@apollo/client";
+import { useParams } from "react-router-dom";
+import React from "react";
+import {
+  RealisationSliderApres,
+  RealisationSliderAvant,
+} from "../components/realisation/RealisationSliderPage";
+
+const GET_REALISATION = gql`
+  query RealisationContent($slug: ID!) {
+    realisation(id: $slug, idType: SLUG) {
+      slug
+      title
+      id
+      realisation {
+        paragrapheSurLeDebutDeLaRealisation
+        paragrapheSurLaRealisation
+      }
+    }
+  }
+`;
+export default function RealisationPage() {
+  const { slug } = useParams();
+
+  const { loading, error, data } = useQuery(GET_REALISATION, {
+    variables: { slug },
+  });
+
+  if (loading)
+    return (
+      <p className="text-3xl mt-3 text-[#f8f8f8] text-center">
+        Loading content…
+      </p>
+    );
+
+  if (error) {
+    return (
+      <p className="text-3xl mt-3 text-[#f8f8f8] text-center">
+        Erreur: {error.message}
+      </p>
+    );
+  }
+
+  const realisation = data.realisation;
+
+  if (!realisation || !realisation.realisation) {
+    return (
+      <p className="text-3xl mt-3 text-[#f8f8f8] text-center">
+        Réalisation introuvable
+      </p>
+    );
+  }
+
+  return (
+    <section className="w-[280px] mt-8 mx-auto text-[#f8f8f8] 2xl:max-w-screen-2xl xl:max-w-screen-lg md:max-w-[700px]  sm:container">
+      <h1 className="text-3xl font-semibold text-center 2xl:max-w-[45vw] 2xl:text-[2.5rem] xl:text-start xl:max-w-[75vw] md:text-5xl">
+        {realisation.title}
+      </h1>
+      <h4 className="text-xl mt-12 2xl:text-3xl xl:text-2xl xl:mt-24 ">
+        Avant le projet :
+      </h4>
+      <RealisationSliderAvant />
+      {realisation.realisation.paragrapheSurLeDebutDeLaRealisation && (
+        <p className="max-w-[270px] mt-7 2xl:max-w-screen-2xl 2xl:mt-28 2xl:text-xl xl:max-w-screen-xl xl:mt-20 lg:max-w-screen-lg lg:mt-14 md:max-w-screen-md md:mt-9  sm:max-w-screen-sm">
+          {realisation.realisation.paragrapheSurLeDebutDeLaRealisation
+            .split("\n")
+            .map((line, index) => (
+              <React.Fragment key={index}>
+                {index > 0 && <br />} {line}
+              </React.Fragment>
+            ))}
+        </p>
+      )}
+      <h4 className="text-xl mt-12 2xl:text-3xl xl:text-2xl xl:mt-24">
+        Avant le projet :
+      </h4>
+      <RealisationSliderApres />
+      {realisation.realisation.paragrapheSurLaRealisation && (
+        <p className="max-w-[270px] mt-7 2xl:max-w-screen-2xl 2xl:mt-28 2xl:text-xl xl:max-w-screen-xl xl:mt-20 lg:max-w-screen-lg lg:mt-14 md:max-w-screen-md md:mt-9  sm:max-w-screen-sm">
+          {realisation.realisation.paragrapheSurLaRealisation
+            .split("\n")
+            .map((line, index) => (
+              <React.Fragment key={index}>
+                {index > 0 && <br />} {line}
+              </React.Fragment>
+            ))}
+        </p>
+      )}
+    </section>
+  );
+}
