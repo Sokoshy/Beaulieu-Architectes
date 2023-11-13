8f6177afaf0a3a5b564556a2094b6bd1e2b5bad5
Sokoshy <92679596+Sokoshy@users.noreply.github.com>
2023-11-13 16:47:20 +0100

    add ContactComponents

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,9 @@
+import { ContactComponents } from "../components/contact/ContactComponents";
+
+export default function Contact() {
+  return (
+    <>
+      <ContactComponents />
+    </>
+  );
+}
