d171dec4775adab296cf2959ab86b788207d330c
Sokoshy <92679596+Sokoshy@users.noreply.github.com>
2023-11-20 13:59:29 +0100

    Add nav component

--- src/pages/Realisation.jsx
+++ src/pages/Realisation.jsx
@@ -1,8 +1,10 @@
+import Nav from "../components/nav/Nav";
 import { RealisationList } from "../components/realisation/RealisationList";
 
 export default function Realisation() {
   return (
     <>
+      <Nav />
       <RealisationList />
     </>
   );
--- src/pages/RealistionPage.jsx
+++ src/pages/RealistionPage.jsx
@@ -5,6 +5,7 @@ import {
   RealisationSliderApres,
   RealisationSliderAvant,
 } from "../components/realisation/RealisationSliderPage";
+import Nav from "../components/nav/Nav";
 
 const GET_REALISATION = gql`
   query RealisationContent($slug: ID!) {
@@ -52,40 +53,43 @@ export default function RealisationPage() {
   }
 
   return (
-    <section className="w-[280px] mt-8 mx-auto text-[#f8f8f8] 2xl:max-w-screen-2xl xl:max-w-screen-lg md:max-w-[700px]  sm:container">
-      <h1 className="text-3xl font-semibold text-center 2xl:max-w-[45vw] 2xl:text-[2.5rem] xl:text-start xl:max-w-[75vw] md:text-5xl">
-        {realisation.title}
-      </h1>
-      <h4 className="text-xl mt-12 2xl:text-3xl xl:text-2xl xl:mt-24 ">
-        Avant le projet :
-      </h4>
-      <RealisationSliderAvant />
-      {realisation.realisation.paragrapheSurLeDebutDeLaRealisation && (
-        <p className="max-w-[270px] mt-7 2xl:max-w-screen-2xl 2xl:mt-28 2xl:text-xl xl:max-w-screen-xl xl:mt-20 lg:max-w-screen-lg lg:mt-14 md:max-w-screen-md md:mt-9  sm:max-w-screen-sm">
-          {realisation.realisation.paragrapheSurLeDebutDeLaRealisation
-            .split("\n")
-            .map((line, index) => (
-              <React.Fragment key={index}>
-                {index > 0 && <br />} {line}
-              </React.Fragment>
-            ))}
-        </p>
-      )}
-      <h4 className="text-xl mt-12 2xl:text-3xl xl:text-2xl xl:mt-24">
-        Avant le projet :
-      </h4>
-      <RealisationSliderApres />
-      {realisation.realisation.paragrapheSurLaRealisation && (
-        <p className="max-w-[270px] mt-7 2xl:max-w-screen-2xl 2xl:mt-28 2xl:text-xl xl:max-w-screen-xl xl:mt-20 lg:max-w-screen-lg lg:mt-14 md:max-w-screen-md md:mt-9  sm:max-w-screen-sm">
-          {realisation.realisation.paragrapheSurLaRealisation
-            .split("\n")
-            .map((line, index) => (
-              <React.Fragment key={index}>
-                {index > 0 && <br />} {line}
-              </React.Fragment>
-            ))}
-        </p>
-      )}
-    </section>
+    <>
+      <Nav />
+      <section className="w-[280px] mx-auto text-[#f8f8f8] 2xl:max-w-screen-2xl xl:max-w-screen-lg md:max-w-[700px]  sm:container">
+        <h1 className="text-3xl font-semibold mt-20 text-center 2xl:max-w-[45vw] 2xl:text-[2.5rem] xl:text-start xl:max-w-[75vw] md:text-5xl">
+          {realisation.title}
+        </h1>
+        <h4 className="text-xl mt-12 2xl:text-3xl xl:text-2xl xl:mt-24 ">
+          Avant le projet :
+        </h4>
+        <RealisationSliderAvant />
+        {realisation.realisation.paragrapheSurLeDebutDeLaRealisation && (
+          <p className="max-w-[270px] mt-7 2xl:max-w-screen-2xl 2xl:mt-28 2xl:text-xl xl:max-w-screen-xl xl:mt-20 lg:max-w-screen-lg lg:mt-14 md:max-w-screen-md md:mt-9  sm:max-w-screen-sm">
+            {realisation.realisation.paragrapheSurLeDebutDeLaRealisation
+              .split("\n")
+              .map((line, index) => (
+                <React.Fragment key={index}>
+                  {index > 0 && <br />} {line}
+                </React.Fragment>
+              ))}
+          </p>
+        )}
+        <h4 className="text-xl mt-12 2xl:text-3xl xl:text-2xl xl:mt-24">
+          Avant le projet :
+        </h4>
+        <RealisationSliderApres />
+        {realisation.realisation.paragrapheSurLaRealisation && (
+          <p className="max-w-[270px] mt-7 2xl:max-w-screen-2xl 2xl:mt-28 2xl:text-xl xl:max-w-screen-xl xl:mt-20 lg:max-w-screen-lg lg:mt-14 md:max-w-screen-md md:mt-9  sm:max-w-screen-sm">
+            {realisation.realisation.paragrapheSurLaRealisation
+              .split("\n")
+              .map((line, index) => (
+                <React.Fragment key={index}>
+                  {index > 0 && <br />} {line}
+                </React.Fragment>
+              ))}
+          </p>
+        )}
+      </section>
+    </>
   );
 }
